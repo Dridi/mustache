17cd7fd54a20db1cdb455c7d36b0738583283edc
Dridi <dridi.b@laposte.net>
2011-08-07 17:11:09 +0200

    added independant interpolation mechanisms

---
+++ core/src/main/java/mustache/Context.java
@@ -0,0 +1,183 @@
+package mustache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The <code>Context</code> class is a wrapper providing a facility for Mustache
+ * {@link Interpolation}. It offers a means to querying a specific value in a
+ * complex graph of objects. The <code>Context</code> itself only holds the root
+ * object of the graph.
+ * 
+ * <h4>Interpolation</h4>
+ * 
+ * <p>
+ * Interpolation has two distinct behaviours :
+ * <ol>
+ *  <li>returning the root object for {@link #SELF} queries</li>
+ *  <li>computing {@link Interpolation} otherwise</li>
+ * </ol>
+ * </p>
+ * 
+ * <h4>Behaviour</h4>
+ * 
+ * <p>
+ * Though instances of this class are themself <i>immutable</i>, it might not be
+ * the case for data they're holding. Those data might not even be suitable for
+ * <i>concurrent access</i>. It is therefore not recommended to share instances
+ * among multiple threads. One should also be aware that instances of this class
+ * hold <i>strong references</i> to interpolated data. Retaining such instances
+ * might lead to memory leaks if not handled with care.
+ * </p>
+ * 
+ * @author Dri
+ */
+public class Context {
+	
+	/**
+	 * The interpolation representing actual data wrapped by the <code>Context</code>.
+	 */
+	public static final String SELF = ".";
+	
+	private final Object data;
+	
+	private Context(Object data) {
+		this.data = data;
+	}
+	
+	/**
+	 * A factory for creating a <code>Context</code> holding <code>data</code>.
+	 * 
+	 * @param data data wrapped by the <code>Context</code>
+	 * @return a newly created <code>Context</code>
+	 */
+	public static Context newInstance(Object data) {
+		return isBasic(data) ? new BasicContext(data) : new Context(data);
+	}
+	
+	/**
+	 * A factory for creating a {@link List} of <code>Context</code> holding <code>data</code>.
+	 * 
+	 * @param data a list of data wrapped by the <code>Context</code> instances
+	 * @return a {@link List} of newly created <code>Context</code>
+	 */
+	public static List<Context> newInstances(List<?> data) {
+		
+		if (data == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		List<Context> contexts = new ArrayList<Context>( data.size() );
+		
+		for (Object item : data) {
+			contexts.add( newInstance(item) );
+		}
+		
+		return contexts;
+	}
+	
+	/**
+	 * Returns <code>true</code> if the query is a valid interpolation. In a
+	 * <code>Context</code>, {@link #SELF} is a valid query.
+	 * 
+	 * @param query the query to check
+	 * @return <code>true</code> if the query is valid
+	 * @see Interpolation#isValidQuery(String)
+	 */
+	public static boolean isValidQuery(String query) {
+		return SELF.equals(query) || Interpolation.isValidQuery(query);
+	}
+	
+	/**
+	 * This method interpolates a value within the context unless the query equals
+	 * to {@link #SELF}.
+	 * 
+	 * @param query the context query
+	 * @return the interpolated value or <code>null</code> if it failed
+	 * @throws IllegalArgumentException if the query is not valid
+	 * @see Interpolation#interpolate(String, Object)
+	 */
+	public Object interpolate(String query) {
+		
+		if ( SELF.equals(query) ) {
+			return data;
+		}
+		
+		return Interpolation.interpolate(query, data);
+	}
+	
+	/**
+	 * Indicates whether the first part of the query matches a variable in the
+	 * context's root. A {@link #SELF} query shall always return <code>true</code>
+	 * since it matches the root itself.
+	 * 
+	 * @param query the context query
+	 * @return <code>true</code> if there is a base variable matching the query
+	 * @throws IllegalArgumentException if the query is not valid
+	 * @see Interpolation#hasBaseVariable(String, Object)
+	 */
+	public boolean hasBaseVariable(String query) {
+		
+		if ( SELF.equals(query) ) {
+			return true;
+		}
+		
+		return Interpolation.hasBaseVariable(query, data);
+	}
+	
+	private static final Class<?>[] BASIC_CLASSES = {String.class, Number.class, Boolean.class};
+	
+	private static boolean isBasic(Object data) {
+		
+		if (data == null) {
+			return true;
+		}
+		
+		for (Class<?> clazz : BASIC_CLASSES) {
+			if ( clazz.isInstance(data) ) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * The <code>BasicContext</code> class is a simplified {@link Context} for basic types.
+	 * Basic types are types for which interpolation other than {@link #SELF} is irrelevant.
+	 */
+	static class BasicContext extends Context {
+		
+		private BasicContext(Object data) {
+			super(data);
+		}
+		
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Object interpolate(String query) {
+			
+			if ( SELF.equals(query) ) {
+				return super.data;
+			}
+			
+			Interpolation.checkQuery(query);
+			return null;
+		}
+		
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public boolean hasBaseVariable(String query) {
+			
+			if ( SELF.equals(query) ) {
+				return true;
+			}
+			
+			Interpolation.checkQuery(query);
+			return false;
+		}
+	}
+}
---
+++ core/src/main/java/mustache/Interpolation.java
@@ -0,0 +1,155 @@
+package mustache;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * This is a utility class for {@link Mustache} interpolation. It works with
+ * queries applied on objects. A query is a string with dot-separated variable
+ * names.
+ * 
+ * 
+ * <p>
+ * Dot-separated variable names look like <code>dog.body.tail</code> where dog
+ * would be a property of the root object. Variables can be object fields,
+ * no-arg methods and {@link Map} keys. If interpolation fails at some point,
+ * <code>null</code> is returned. The <code>Interpolation</code> itself does
+ * <i>not</i> coerce <code>null</code> or falsey values into empty strings. This
+ * feature is only needed for actual {@link Mustache} rendering.
+ * </p>
+ * 
+ * <p>Valid interpolations are matched against {@value #QUERY_REGEX}.</p>
+ * 
+ * TODO interpolate methods (currently not implemented)
+ * TODO interpolate in getters ? (only fields are currently supported)
+ * 
+ * @author Dri
+ * @see Context
+ */
+public final class Interpolation {
+	
+	/**
+	 * The separator of the nested variable names in queries.
+	 */
+	public static final String SEPARATOR = ".";
+	
+	private static final Pattern SEPARATOR_PATTERN = Pattern.compile( Pattern.quote(SEPARATOR) );
+
+	/**
+	 * The regex for valid interpolation queries. The fact that variable interpolation might
+	 * fail given not-existing variable names within data is irrelevant for validation.
+	 */
+	public static final String QUERY_REGEX = "^[a-z_][a-z0-9_]*(\\.[a-z_][a-z0-9_]*)*$";
+	
+	private static final Pattern QUERY_PATTERN = Pattern.compile(QUERY_REGEX, Pattern.CASE_INSENSITIVE);
+	
+	private Interpolation() {}
+	
+	/**
+	 * Checks whether a quey is valid.
+	 * @param query the query to check
+	 * @throws IllegalArgumentException if the query is not valid
+	 */
+	static void checkQuery(String query) {
+		if ( !isValidQuery(query) ) {
+			throw new IllegalArgumentException("Invalid query : " + query);
+		}
+	}
+	
+	/**
+	 * Returns <code>true</code> if the query is valid.
+	 * @param query the query to check
+	 * @return <code>true</code> if the query is valid
+	 */
+	public static boolean isValidQuery(String query) {
+		return QUERY_PATTERN.matcher(query).matches();
+	}
+	
+	/**
+	 * This method looks up the query's variables in the object graph. It returns the value
+	 * of the last variable unless a variable wasn't found at some point. If the interpolation
+	 * failed, it returns <code>null</code>, but returning <code>null</code> does not necessary
+	 * mean failure.
+	 * 
+	 * @param query the context query
+	 * @param object the object graph
+	 * @return the interpolated value or <code>null</code> if it failed
+	 * @throws IllegalArgumentException if the query is not valid
+	 */
+	public static Object interpolate(String query, Object object) {
+		
+		checkQuery(query);
+		
+		Object value = object;
+		String[] names = SEPARATOR_PATTERN.split(query);
+		
+		for (String name : names) {
+			
+			if ( !hasBaseVariable(name, value) ) {
+				return null;
+			}
+			
+			value = getValue(name, value);
+		}
+		
+		return value;
+	}
+	
+	private static Object getValue(String name, Object object) {
+		
+		if (object instanceof Map) {
+			return ((Map<?, ?>) object).get(name);
+		}
+		
+		try {
+			Field field = object.getClass().getDeclaredField(name);
+			field.setAccessible(true);
+			return field.get(object);
+		}
+		catch (NoSuchFieldException e) {
+			return null;
+		}
+		catch (IllegalAccessException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+	
+	/**
+	 * Indicates whether their is a base variable matching the query in the object graph.
+	 * 
+	 * <p>
+	 * Querying <code>dog.body.tail</code> will return <code>true</code> only if there
+	 * is a <code>dog</code> variable in <code>object</code>.
+	 * </p>
+	 * 
+	 * @param query the context query
+	 * @param object the object graph
+	 * @return <code>true</code> if there is a base variable matching the query
+	 * @throws IllegalArgumentException if the query is not valid
+	 */
+	public static boolean hasBaseVariable(String query, Object object) {
+
+		checkQuery(query);
+		
+		if (object == null || object.getClass().isArray() || object instanceof Collection) {
+			return false;
+		}
+		
+		String baseName = SEPARATOR_PATTERN.split(query, 2)[0];
+		
+		if (object instanceof Map) {
+			return ((Map<?, ?>) object).containsKey(baseName);
+		}
+		
+		try {
+			object.getClass().getDeclaredField(baseName);
+			return true;
+		}
+		catch (NoSuchFieldException e) {
+			return false;
+		}
+	}
+
+}
