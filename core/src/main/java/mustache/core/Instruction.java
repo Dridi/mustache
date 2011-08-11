fc08aa4901dd02f0543d0f459e39ee43dd6833fe
Dridi <dridi.b@laposte.net>
2011-08-11 22:38:51 +0200

    added and documented serializable core class Instruction

---
+++ core/src/main/java/mustache/core/Instruction.java
@@ -0,0 +1,100 @@
+package mustache.core;
+
+import java.io.InvalidObjectException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/**
+ * This class represents a {@link Mustache} instruction. An {@code Instruction}
+ * is basically represented by an action and data associated to it. Instances of
+ * this class are immutable and can be shared safely among multiple threads.
+ * 
+ * @author Dri
+ */
+public final class Instruction implements Serializable {
+	
+	private static final long serialVersionUID = 7976280972290440553L;
+	
+	/**
+	 * A reusable <i>no operation performed</i> {@code Instruction}.
+	 */
+	public static final Instruction NOP = new Instruction(Type.NOP, null);
+	
+	/**
+	 * The set of available instructions.
+	 */
+	public enum Type {
+		NOP,
+		APPEND_TEXT,
+		APPEND_VARIABLE,
+		APPEND_UNESCAPED_VARIABLE,
+		OPEN_SECTION,
+		OPEN_INVERTED_SECTION,
+		CLOSE_SECTION;
+	}
+	
+	private final Type type;
+	private final String data;
+
+	private Instruction(Type type, String data) {
+		this.type = type;
+		this.data = data;
+	}
+	
+	/**
+	 * Creates a new {@code Instruction}.
+	 * @param type the type of {@code Instruction}
+	 * @param data the data associated to the {@code Instruction}
+	 * @return a newly created {@code Instruction}
+	 * @throws NullPointerException if {@code type} or {@code data} is {@code null}
+	 */
+	public static Instruction newInstance(Type type, String data) {
+		if (type == Type.NOP) {
+			return NOP;
+		}
+		
+		if (type == null || data == null) {
+			throw new NullPointerException();
+		}
+		
+		return new Instruction(type, data);
+	}
+	
+	/**
+	 * @return the {@code Instruction} {@link Type}
+	 */
+	public Type getType() {
+		return type;
+	}
+	
+	/**
+	 * @return the {@code Instruction} data
+	 */
+	public String getData() {
+		return data;
+	}
+
+	private Object writeReplace() {
+		return new SerializationProxy(this);
+	}
+
+	private void readObject(ObjectInputStream stream) throws InvalidObjectException {
+		throw new InvalidObjectException("Proxy required");
+	}
+
+	private static class SerializationProxy implements Serializable {
+		private static final long serialVersionUID = -1303026970090245725L;
+		
+		private final Type type;
+		private final String data;
+
+		SerializationProxy(Instruction instruction) {
+			this.type = instruction.type;
+			this.data = instruction.data;
+		}
+		
+		private Object readResolve() {
+			return Instruction.newInstance(type, data);
+		}
+	}
+}
