c410a8a60532d5b121ce7f4b701712d3bb85fa19
Dridi <dridi.b@laposte.net>
2011-09-01 00:38:39 +0200

    fixed support for inverted sections

--- core/src/main/java/mustache/core/SectionStack.java
+++ core/src/main/java/mustache/core/SectionStack.java
@@ -12,17 +12,17 @@ public class SectionStack {
 		this.sections.push( Section.rootSection(root) );
 	}
 	
-	private Section findSection(String query) {
+	private Section findSection(String query, boolean inverted) {
 		for (Section section : sections) {
 			if (section.hasBaseVariable(query)) {
 				return section;
 			}
 		}
-		return null;
+		return inverted ? sections.element() : null;
 	}
 	
 	public String getValue(String query) {
-		Section section = findSection(query);
+		Section section = findSection(query, false);
 		if (section == null) {
 			return "";
 		}
@@ -31,7 +31,7 @@ public class SectionStack {
 	}
 
 	private boolean openSection(String query, boolean inverted) {
-		Section section = findSection(query);
+		Section section = findSection(query, inverted);
 		if (section == null) {
 			return false;
 		}
