af5bf2cde1ce9b7a867e91b945b2e15be1808452
Dridi <dridi.b@laposte.net>
2011-09-04 20:10:17 +0200

    added serial version uid to ParseException

--- parser/src/main/java/mustache/parser/ParseException.java
+++ parser/src/main/java/mustache/parser/ParseException.java
@@ -1,7 +1,8 @@
 package mustache.parser;
 
 public final class ParseException extends Exception {
-	
+	private static final long serialVersionUID = 7245280982537468242L;
+
 	ParseException(String message) {
 		super(message);
 	}
